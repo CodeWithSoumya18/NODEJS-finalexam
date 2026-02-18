93502159d94d53888daa67c635e0b19804b0b3b5
Soumyashree Nayak <8391soumyanayak@gmail.com>
2026-02-18 11:18:57 +0530

    Update server.js

--- Ecommerce/server.js
+++ Ecommerce/server.js
@@ -1,22 +1,25 @@
 import express from "express"
-import bodyParser from "body-parser"
-import mongoose, { connect } from "mongoose"
 import cookieParser from "cookie-parser"
 import dotenv from "dotenv"
 import connectDB from "./config/db.js"
 
-
-import authRoutes from "./routes/authRoutes.js"
+import authRoutes from "./routes/auth.routes.js"
+import productRoutes from "./routes/product.routes.js"
+import categoryRoutes from "./routes/category.routes.js"
 
 dotenv.config()
 connectDB()
 
-const app= express()
-app.use (cookieParser())
-app.use( express.urlencoded({extended:true}))
+const app = express()
 
-app.use ("/",authRoutes)
+app.set("view engine", "ejs")
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 
+app.use("/", authRoutes)
+app.use("/products", productRoutes)
+app.use("/categories", categoryRoutes)
 
-app.listen(4000,()=>
-console.log("server running on 4000 !"))
+app.listen(3000, () =>
+  console.log("🚀 Server running on http://localhost:3000")
+)
