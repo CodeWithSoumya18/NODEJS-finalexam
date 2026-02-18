627a8d9bc230ec2e25d376c8dcf075b432f29b39
Soumyashree Nayak <8391soumyanayak@gmail.com>
2026-02-18 11:24:36 +0530

    Update import paths for product and category routes

--- Ecommerce/server.js
+++ Ecommerce/server.js
@@ -4,15 +4,15 @@ import dotenv from "dotenv"
 import connectDB from "./config/db.js"
 
 import authRoutes from "./routes/auth.routes.js"
-import productRoutes from "./routes/product.routes.js"
-import categoryRoutes from "./routes/category.routes.js"
+import productRoutes from "./routes/productRoutes.js"
+import categoryRoutes from "./routes/categoryRoutes.js"
 
 dotenv.config()
 connectDB()
 
 const app = express()
 
-app.set("view engine", "ejs")
+
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
