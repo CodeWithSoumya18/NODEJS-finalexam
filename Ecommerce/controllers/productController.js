bb7f63b36ae26588026fd11811dae6686dfecb3e
Soumyashree Nayak <8391soumyanayak@gmail.com>
2026-02-18 11:22:03 +0530

    Update productController.js

---
+++ Ecommerce/controllers/productController.js
@@ -0,0 +1,67 @@
+import Product from "../models/product.model.js"
+import Category from "../models/category.model.js"
+
+
+export const allProducts = async (req, res) => {
+  try {
+    const products = await Product.find()
+      .populate("category", "name")
+      .populate("user", "username")
+
+    res.render("productList", { products })
+  } catch (err) {
+    res.status(500).send("Failed to fetch products")
+  }
+}
+
+
+export const myProducts = async (req, res) => {
+  try {
+    const products = await Product.find({ user: req.user.id })
+      .populate("category", "name")
+
+    res.render("myProducts", { products })
+  } catch (err) {
+    res.status(500).send("Failed to fetch user products")
+  }
+}
+
+
+export const productForm = async (req, res) => {
+  try {
+    const categories = await Category.find()
+    res.render("productForm", { categories })
+  } catch (err) {
+    res.status(500).send("Failed to load form")
+  }
+}
+
+
+export const addProduct = async (req, res) => {
+  try {
+    const { name, price, category } = req.body
+
+    await Product.create({
+      name,
+      price,
+      category,
+      user: req.user.id
+    })
+
+    res.redirect("/products")
+  } catch (err) {
+    res.status(500).send("Product creation failed")
+  }
+}
+
+export const productsByCategory = async (req, res) => {
+  try {
+    const products = await Product.find({
+      category: req.params.categoryId
+    }).populate("category", "name")
+
+    res.render("productList", { products })
+  } catch (err) {
+    res.status(500).send("Category filter failed")
+  }
+}
